530116eaecf8f0f211f4c452b7d73e20e2bb687c
sbhandari1992 <sambhandari2021@yahoo.com>
2023-06-15 17:08:04 -0400

    create responsive contact form and display info

---
+++ hamro-hotel/src/app/components/contact/contact.interface.ts
@@ -0,0 +1,8 @@
+export interface ContactInfoItem {
+  content: string;
+  icon: string;
+}
+
+export interface ContactInfo {
+  contactInfo: ContactInfoItem[];
+}
--- hamro-hotel/src/app/services/content.services.ts
+++ hamro-hotel/src/app/services/content.services.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import * as navData from '../../assets/json/nav.json';
 import * as loginInfoData from '../../assets/json/login-info.json';
 import * as fancyWordData from '../../assets/json/fancy-word.json';
+import * as contactInfoData from '../../assets/json/contact-info.json'
 
 @Injectable({
   providedIn: 'root',
@@ -10,6 +11,8 @@ export class ContentService {
   private navItemsJSON = navData;
   private loginInfoJSON = loginInfoData;
   private fancyWordsJSON = fancyWordData;
+  private contactInfoJSON = contactInfoData;
+
 
   constructor() {}
 
@@ -22,4 +25,27 @@ export class ContentService {
   get fancyWords() {
     return this.fancyWordsJSON;
   }
+  // get contactInfo() {
+  //   return this.contactInfoJSON;
+  // }
+  get contactInfo() {
+    return this.contactInfoJSON.contactInfo;
+  }
 }
+
+
+
+// import contactInfoData from '../../assets/json/contact-info.json';
+
+// @Injectable({
+//   providedIn: 'root',
+// })
+// export class ContentService {
+//   // private contactInfoJSON = contactInfoData;
+
+//   constructor() {}
+
+//   get contactInfo() {
+//     return this.contactInfoJSON.contactInfo;
+//   }
+// }
