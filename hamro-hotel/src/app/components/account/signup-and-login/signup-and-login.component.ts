b0a3c94ef9eaeec597a9125fb7f4d25e76c96b91
sbhandari1992 <sambhandari2021@yahoo.com>
2023-06-20 16:35:08 -0400

    testomonials works fine now it wasn't working properly before

--- hamro-hotel/src/app/components/account/signup-and-login/signup-and-login.component.ts
+++ hamro-hotel/src/app/components/account/signup-and-login/signup-and-login.component.ts
@@ -5,71 +5,70 @@ import { Router } from '@angular/router';
 // import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
-  selector: 'app-signup-and-login',
-  templateUrl: './signup-and-login.component.html',
-  styleUrls: ['./signup-and-login.component.scss']
+    selector: 'app-signup-and-login',
+    templateUrl: './signup-and-login.component.html',
+    styleUrls: ['./signup-and-login.component.scss'],
 })
 export class SignupAndLoginComponent implements OnInit {
-  isButtonClicked: boolean = false;
-  isSignUpFormVisible: boolean = false;
+    isButtonClicked: boolean = false;
+    isSignUpFormVisible: boolean = false;
 
-  showSignUpForm(): void {
-    this.isSignUpFormVisible = !this.isSignUpFormVisible;
-    console.log('true now');
-  }
+    showSignUpForm(): void {
+        this.isSignUpFormVisible = !this.isSignUpFormVisible;
+        console.log('true now');
+    }
 
+    loginForm = this.fb.group({
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', Validators.required],
+    });
+    signUpForm = this.fb.group({
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', Validators.required],
+    });
 
-  loginForm = this.fb.group({
-    email: ['', [Validators.required, Validators.email]],
-    password: ['', Validators.required],
-  });
-  signUpForm = this.fb.group({
-    email: ['', [Validators.required, Validators.email]],
-    password: ['', Validators.required],
-  });
+    constructor(
+        // private authService: AuthService,
+        // private toast: HotToastService,
+        private router: Router,
+        private fb: NonNullableFormBuilder
+    ) {}
 
-  constructor(
-    // private authService: AuthService,
-    // private toast: HotToastService,
-    private router: Router,
-    private fb: NonNullableFormBuilder
-  ) {}
+    ngOnInit(): void {}
 
-  ngOnInit(): void {}
+    get email() {
+        return this.loginForm.get('email');
+    }
 
-  get email() {
-    return this.loginForm.get('email');
-  }
+    get password() {
+        return this.loginForm.get('password');
+    }
 
-  get password() {
-    return this.loginForm.get('password');
-  }
+    openRightPanel() {
+        this.isButtonClicked = !this.isButtonClicked;
+    }
+    openLeftPanel() {
+        this.isButtonClicked = !this.isButtonClicked;
+    }
 
-  openRightPanel() {
-    this.isButtonClicked = !this.isButtonClicked;
-  }
-  openLeftPanel() {
-    this.isButtonClicked = !this.isButtonClicked;
-  }
+    // submit() {
+    //   const { email, password } = this.loginForm.value;
 
-  // submit() {
-  //   const { email, password } = this.loginForm.value;
+    //   if (!this.loginForm.valid || !email || !password) {
+    //     return;
+    //   }
 
-  //   if (!this.loginForm.valid || !email || !password) {
-  //     return;
-  //   }
-
-  //   this.authService
-  //     .login(email, password)
-  //     .pipe(
-  //       this.toast.observe({
-  //         success: 'Logged in successfully',
-  //         loading: 'Logging in...',
-  //         error: ({ message }) => `There was an error: ${message} `,
-  //       })
-  //     )
-  //     .subscribe(() => {
-  //       this.router.navigate(['/home']);
-  //     });
-  // }
+    //   this.authService
+    //     .login(email, password)
+    //     .pipe(
+    //       this.toast.observe({
+    //         success: 'Logged in successfully',
+    //         loading: 'Logging in...',
+    //         error: ({ message }) => `There was an error: ${message} `,
+    //       })
+    //     )
+    //     .subscribe(() => {
+    //       this.router.navigate(['/home']);
+    //     });
+    // }
 }
--- hamro-hotel/src/app/components/home/testimony/testimony.component.ts
+++ hamro-hotel/src/app/components/home/testimony/testimony.component.ts
@@ -5,120 +5,124 @@ import { Testimonial } from '../../../modules/testimonial.model';
 import { TestimonialService } from '../../../services/testimonial.service';
 
 @Component({
-  selector: 'app-testimony',
-  templateUrl: './testimony.component.html',
-  styleUrls: ['./testimony.component.scss'],
+    selector: 'app-testimony',
+    templateUrl: './testimony.component.html',
+    styleUrls: ['./testimony.component.scss'],
 })
 export class TestimonyComponent implements OnInit, OnDestroy {
-  testimonials: Testimonial[] = [];
-  visibleTestimonials: Testimonial[] = [];
-  currentIndex = 0;
-  private destroy$ = new Subject<void>();
-  private intervalSubscription: Subscription;
-  animationEnabled = true;
-  isMobile = false;
+    testimonials: Testimonial[] = [];
+    visibleTestimonials: Testimonial[] = [];
+    currentIndex = 0;
+    private destroy$ = new Subject<void>();
+    private intervalSubscription: Subscription;
+    animationEnabled = true;
+    isMobile = false;
 
-  constructor(private testimonialService: TestimonialService) {}
+    constructor(private testimonialService: TestimonialService) {}
 
-  ngOnInit(): void {
-    this.checkViewportSize();
-    this.testimonialService.getTestimonials().subscribe((testimonials) => {
-      this.testimonials = testimonials;
-      this.updateVisibleTestimonials();
-      this.startInterval();
-    });
-  }
-
-  ngOnDestroy(): void {
-    this.destroy$.next();
-    this.destroy$.complete();
-    this.clearInterval();
-  }
-
-  @HostListener('window:resize')
-  checkViewportSize(): void {
-    const previousIsMobile = this.isMobile;
-    this.isMobile = window.innerWidth < 1000;
-
-    if (previousIsMobile !== this.isMobile) {
-      this.clearInterval();
-      this.updateVisibleTestimonials();
-      this.resetInterval();
+    ngOnInit(): void {
+        this.checkViewportSize();
+        this.testimonialService.getTestimonials().subscribe((testimonials) => {
+            this.testimonials = testimonials;
+            this.updateVisibleTestimonials();
+            this.startInterval();
+        });
     }
-  }
 
-  updateVisibleTestimonials(): void {
-    const count = this.isMobile ? 1 : 2;
-    const currentIndex = this.currentIndex;
-    this.visibleTestimonials.splice(0);
-    const randomIndexes = this.generateRandomIndexes(currentIndex, this.testimonials.length, count);
-    randomIndexes.forEach((index) => {
-      this.visibleTestimonials.push({ ...this.testimonials[index] } as Testimonial);
-    });
-  }
-
-  slideLeft(): void {
-    this.currentIndex = (this.currentIndex - 1 + this.testimonials.length) % this.testimonials.length;
-    this.updateVisibleTestimonials();
-    this.resetInterval();
-  }
-
-  slideRight(): void {
-    this.currentIndex = (this.currentIndex + 1) % this.testimonials.length;
-    this.updateVisibleTestimonials();
-    this.resetInterval();
-  }
-
-  startInterval(): void {
-    this.intervalSubscription = interval(7000)
-      .pipe(takeUntil(this.destroy$))
-      .subscribe(() => {
-        this.slideRight();
-      });
-  }
-
-  clearInterval(): void {
-    if (this.intervalSubscription) {
-      this.intervalSubscription.unsubscribe();
+    ngOnDestroy(): void {
+        this.destroy$.next();
+        this.destroy$.complete();
+        this.clearInterval();
     }
-  }
 
-  resetInterval(): void {
-    this.clearInterval();
-    this.startInterval();
-  }
+    @HostListener('window:resize')
+    checkViewportSize(): void {
+        const previousIsMobile = this.isMobile;
+        this.isMobile = window.innerWidth < 1000;
 
-  onMouseEnterTimeout: any;
-
-  onMouseEnter(): void {
-    console.log('Mouse entered');
-    this.clearInterval();
-
-    this.onMouseEnterTimeout = setTimeout(() => {
-      this.animationEnabled = false;
-    }, 2000);
-  }
-
-  onMouseLeave(): void {
-    console.log('Mouse left');
-    clearTimeout(this.onMouseEnterTimeout);
-    this.animationEnabled = true;
-    this.startInterval();
-  }
-
-  generateRandomIndexes(currentIndex: number, length: number, count: number): number[] {
-    const availableIndexes = Array.from({ length }, (_, i) => i);
-    const remainingIndexes = availableIndexes.filter((index) => index !== currentIndex);
-    const shuffledIndexes = this.shuffleArray(remainingIndexes);
-    return shuffledIndexes.slice(0, count);
-  }
-
-  shuffleArray(array: any[]): any[] {
-    const newArray = array.slice();
-    for (let i = newArray.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+        if (previousIsMobile !== this.isMobile) {
+            this.clearInterval();
+            this.updateVisibleTestimonials();
+            this.resetInterval();
+        }
     }
-    return newArray;
-  }
+
+    updateVisibleTestimonials(): void {
+        const count = this.isMobile ? 1 : 2;
+        const currentIndex = this.currentIndex;
+        this.visibleTestimonials.splice(0);
+        const randomIndexes = this.generateRandomIndexes(currentIndex, this.testimonials.length, count);
+        randomIndexes.forEach((index) => {
+            this.visibleTestimonials.push({ ...this.testimonials[index] } as Testimonial);
+        });
+    }
+
+    slideLeft(): void {
+        this.currentIndex = (this.currentIndex - 1 + this.testimonials.length) % this.testimonials.length;
+        this.updateVisibleTestimonials();
+        this.resetInterval();
+    }
+
+    slideRight(): void {
+        this.currentIndex = (this.currentIndex + 1) % this.testimonials.length;
+        this.updateVisibleTestimonials();
+        this.resetInterval();
+    }
+
+    startInterval(): void {
+        this.intervalSubscription = interval(7000)
+            .pipe(takeUntil(this.destroy$))
+            .subscribe(() => {
+                if (this.animationEnabled) {
+                    this.slideRight();
+                }
+            });
+    }
+
+    clearInterval(): void {
+        if (this.intervalSubscription) {
+            this.intervalSubscription.unsubscribe();
+        }
+    }
+
+    resetInterval(): void {
+        this.clearInterval();
+        this.startInterval();
+    }
+
+    onMouseEnterTimeout: any;
+
+    onMouseEnter(): void {
+        this.clearInterval();
+        this.onMouseEnterTimeout = setTimeout(() => {
+            this.animationEnabled = false;
+        }, 2000);
+    }
+
+    onMouseLeave(): void {
+        clearTimeout(this.onMouseEnterTimeout);
+        this.animationEnabled = true;
+        this.startInterval();
+    }
+
+    onTouchStart(): void {
+        this.clearInterval();
+        this.animationEnabled = false;
+    }
+
+    generateRandomIndexes(currentIndex: number, length: number, count: number): number[] {
+        const availableIndexes = Array.from({ length }, (_, i) => i);
+        const remainingIndexes = availableIndexes.filter((index) => index !== currentIndex);
+        const shuffledIndexes = this.shuffleArray(remainingIndexes);
+        return shuffledIndexes.slice(0, count);
+    }
+
+    shuffleArray(array: any[]): any[] {
+        const newArray = array.slice();
+        for (let i = newArray.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+        }
+        return newArray;
+    }
 }
