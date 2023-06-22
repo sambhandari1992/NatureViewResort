0aa239563c3b98d268dbc91f48c96b321bd72cba
sbhandari1992 <sambhandari2021@yahoo.com>
2023-06-22 12:39:00 -0400

    changing branch

--- hamro-hotel/src/app/components/facilitie/facilitie.component.ts
+++ hamro-hotel/src/app/components/facilitie/facilitie.component.ts
@@ -1,45 +1,23 @@
-import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FacilitiesService } from '../../services/facilities.service';
 import { Facility } from '../../models/facilities.interface';
 
 @Component({
-  selector: 'app-facilitie',
-  templateUrl: './facilitie.component.html',
-  styleUrls: ['./facilitie.component.scss'],
+    selector: 'app-facilitie',
+    templateUrl: './facilitie.component.html',
+    styleUrls: ['./facilitie.component.scss'],
 })
-export class FacilitieComponent implements OnInit, AfterViewInit {
-  allFacilities: Facility[] = [];
-  allItems: Facility[] = [];
+export class FacilitieComponent implements OnInit {
+    allItems: Facility[] = [];
+    @Input() applyCustomStyle: boolean = false;
 
-  @ViewChild('boxesContainer') boxesContainer!: ElementRef;
-  @ViewChild('boxElement') boxElements!: ElementRef[];
+    constructor(private facilitiesService: FacilitiesService) {}
 
-  constructor(private facilitiesService: FacilitiesService) {}
-
-  ngOnInit(): void {
-    this.facilitiesService.getFacilities().subscribe((facilities) => {
-      for (let i = 0; i < facilities.length; i++) {
-        this.allItems.push(facilities[i]);
-      }
-    });
-  }
-
-  ngAfterViewInit(): void {
-    this.checkBoxVisibility();
-  }
-
-  checkBoxVisibility(): void {
-    const containerWidth = this.boxesContainer.nativeElement.clientWidth;
-    this.boxElements.forEach((boxElement: ElementRef) => {
-      const boxWidth = boxElement.nativeElement.clientWidth;
-      if (boxWidth > containerWidth) {
-        boxElement.nativeElement.style.display = 'none';
-      } else if (boxWidth < 200) {
-        boxElement.nativeElement.style.display = 'none';
-      } else {
-        boxElement.nativeElement.style.display = 'flex';
-      }
-    });
-  }
-  
+    ngOnInit(): void {
+        this.facilitiesService.getFacilities().subscribe((facilities) => {
+            for (let i = 0; i < facilities.length; i++) {
+                this.allItems.push(facilities[i]);
+            }
+        });
+    }
 }
--- hamro-hotel/src/app/components/home/home.component.ts
+++ hamro-hotel/src/app/components/home/home.component.ts
@@ -1,42 +1,43 @@
 import { Component, OnInit, NgZone } from '@angular/core';
 
 @Component({
-    selector: 'app-home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.scss'],
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-    images: string[] = [];
-    currentImageIndex = 0;
+  images: string[] = [];
+  currentImageIndex = 0;
+  applyCustomStyle: boolean = true;
 
-    constructor(private ngZone: NgZone) {}
+  constructor(private ngZone: NgZone) {}
 
-    ngOnInit() {
-        this.generateImageURLs();
-        this.startImageTimer();
+  ngOnInit() {
+    this.generateImageURLs();
+    this.startImageTimer();
+  }
+
+  generateImageURLs() {
+    const baseURL = '../../../assets/images/home/img-';
+    const totalImages = 21;
+
+    for (let i = 0; i <= totalImages; i++) {
+      const imageURL = `${baseURL}${i}.jpeg`;
+      this.images.push(imageURL);
     }
+  }
 
-    generateImageURLs() {
-        const baseURL = '../../../assets/images/home/img-';
-        const totalImages = 21;
-
-        for (let i = 0; i <= totalImages; i++) {
-            const imageURL = `${baseURL}${i}.jpeg`;
-            this.images.push(imageURL);
-        }
-    }
-
-    startImageTimer() {
-        this.ngZone.runOutsideAngular(() => {
-            setInterval(() => {
-                this.ngZone.run(() => {
-                    this.changeImage();
-                });
-            }, 5000); // Change image every 5 seconds
+  startImageTimer() {
+    this.ngZone.runOutsideAngular(() => {
+      setInterval(() => {
+        this.ngZone.run(() => {
+          this.changeImage();
         });
-    }
+      }, 5000); // Change image every 5 seconds
+    });
+  }
 
-    changeImage() {
-        this.currentImageIndex = (this.currentImageIndex + 1) % this.images.length;
-    }
+  changeImage() {
+    this.currentImageIndex = (this.currentImageIndex + 1) % this.images.length;
+  }
 }
