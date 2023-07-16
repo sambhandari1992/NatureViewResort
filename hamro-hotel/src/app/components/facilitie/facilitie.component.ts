13d52619e4db563811249eb26acb8ced2890d11c
sbhandari1992 <sambhandari2021@yahoo.com>
2023-07-16 16:39:49 -0400

    work to get latest code for main

--- hamro-hotel/src/app/components/facilitie/facilitie.component.ts
+++ hamro-hotel/src/app/components/facilitie/facilitie.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FacilitiesService } from '../../services/facilities.service';
+import { Facility } from '../../models/facilities.interface';
+// import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-facilitie',
     templateUrl: './facilitie.component.html',
     styleUrls: ['./facilitie.component.scss'],
 })
-export class FacilitieComponent {
-   
+export class FacilitieComponent implements OnInit {
+    allItems: Facility[] = [];
+    @Input() customStyle: string = '';
+    @Input() showButton: boolean = false;
+
+    constructor(private facilitiesService: FacilitiesService) {}
+
+    ngOnInit(): void {
+        this.facilitiesService.getFacilities().subscribe((facilities) => {
+            this.allItems = [];
+            for (let i = 0; i < facilities.length; i++) {
+                this.allItems.push(facilities[i]);
+            }
+        });
+    }
 }
