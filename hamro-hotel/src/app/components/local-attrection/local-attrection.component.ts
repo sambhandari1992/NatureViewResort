d8ffe4b12d09abfa54625b3cb797a10f2c9725cd
sbhandari1992 <sambhandari2021@yahoo.com>
2023-07-23 01:37:43 -0400

    complete local-attraction component first stage

--- hamro-hotel/src/app/components/local-attrection/local-attrection.component.ts
+++ hamro-hotel/src/app/components/local-attrection/local-attrection.component.ts
@@ -1,8 +1,31 @@
 import { Component } from '@angular/core';
 
+
+type LocalAttraction = {
+  name: string;
+  description: string;
+  imageUrl: string;
+};
+
 @Component({
   selector: 'app-local-attrection',
   templateUrl: './local-attrection.component.html',
   styleUrls: ['./local-attrection.component.scss'],
 })
-export class LocalAttrectionComponent {}
+export class LocalAttrectionComponent {
+
+  localAttractions: LocalAttraction[] = [];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    for (let i = 1; i <= 20; i++) {
+      this.localAttractions.push({
+        name: `Local Attraction ${i}`,
+        description: `Description of Local Attraction ${i}.`,
+        imageUrl: `https://source.unsplash.com/random/500x500?sig=${i}`,
+      });
+    }
+  }
+
+}
