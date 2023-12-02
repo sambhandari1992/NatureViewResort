368630cfdfe727369dd3cc5e299bcc84b3d45bfb
sbhandari1992 <sambhandari2021@yahoo.com>
2023-12-02 16:42:32 -0500

    saving change

--- hamro-hotel/src/app/components/home/testomonials/testimonial.interface.ts
+++ hamro-hotel/src/app/components/home/testomonials/testimonial.interface.ts
@@ -1,13 +1,22 @@
+// export interface Testimonial {
+//     name: string;
+//     message: string;
+//     profilePicture: string;
+// }
+
+// export interface AnotherInterface {
+//     // Define properties and types for another interface
+// }
+
+// export interface YetAnotherInterface {
+//     // Define properties and types for yet another interface
+// }
+
 export interface Testimonial {
+    id: number;
+    imgUrl: string;
     name: string;
-    message: string;
-    profilePicture: string;
-}
-
-export interface AnotherInterface {
-    // Define properties and types for another interface
-}
-
-export interface YetAnotherInterface {
-    // Define properties and types for yet another interface
+    country: string;
+    description: string;
+    date: string;
 }
